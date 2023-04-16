10803865b8e01753ed797c0ce045d3da4f2cf2bd
XearonMini <4632347@inbox.ru>
2023-04-16 10:16:53 +0000

    Починил локаторы + добавил 2 тестика

--- HW-23/tests/tests.spec.js
+++ HW-23/tests/tests.spec.js
@@ -10,13 +10,18 @@ test('search test', async ({page})=>{
   await page.keyboard.press('Enter');
   await expect(page.locator('text = getByLabel')).toHaveText(['getByLabel']);
 })
-test('Links test', async ({page})=>{
+test('links test', async ({page})=>{
   await page.goto('https://playwright.dev/');
   await page.getByRole('link',{name:'Community'}).click();
   await page.getByText('Live Streams', { exact: true }).click();
-  await expect(page.locator('#docusaurus_skipToContent_fallback')).toContainText('Playwright live stream');
+  await expect(page.locator('#docusaurus_skipToContent_fallback')).toContainText(['Playwright live stream']);
 })
-test('Hover test', async ({page})=>{
+test('List test', async ({page})=>{
+  await page.goto('https://playwright.dev/');
+  await page.locator("#__docusaurus .navbar__item.navbar__link").filter({hasText:'Docs'}).click();
+  await expect(page.locator('#__docusaurus .table-of-contents.table-of-contents__left-border')).toContainText('Test');
+})
+test('hover test', async ({page})=>{
   await page.goto('https://playwright.dev/');
   await page.locator("#__docusaurus .navbar__item.dropdown.dropdown--hoverable").hover();
   await page
@@ -25,8 +30,11 @@ test('Hover test', async ({page})=>{
       .click()
   await expect(page).toHaveTitle(/Playwright .NET/);
 })
-test('List test', async ({page})=>{
+test('Keyboard usage test', async ({page})=>{
+  const searchTable = page.locator('.DocSearch-Modal');
   await page.goto('https://playwright.dev/');
-  await page.locator("#__docusaurus .navbar__item.navbar__link").filter({hasText:'Docs'}).click();
-  await expect(page.locator('#__docusaurus .table-of-contents.table-of-contents__left-border')).toContainText('Test');
+  const SearchButton = page.getByRole('button',{name:'Search '})
+  await SearchButton.click();
+  await page.keyboard.press('Escape');
+  await expect(searchTable).not.toBeVisible();
 })
